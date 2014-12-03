5dee9e738ea8960cfa37069e0ab9f0b779406ea9
crysthianophp <crysthianophp@gmail.com>
2014-12-03 16:41:14 -0200

    estudos apigility

--- data/statuslib.php
+++ data/statuslib.php
@@ -1,4 +1,9 @@
 <?php
 return array(
-
+    '82d28c2a-68ee-423f-84d7-b7bfb33672a7' => array(
+        'message' => 'First post!',
+        'user' => 'mwop',
+        'id' => '82d28c2a-68ee-423f-84d7-b7bfb33672a7',
+        'timestamp' => 1417630231,
+    ),
 );
